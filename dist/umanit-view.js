dd50f210bdab67f669f5ad45740194ab3e913de6
Axel Anceau <axel.anceau@gmail.com>
2015-12-08 15:13:21 +0100

    Fix bug, element added to rootscope

--- dist/umanit-view.js
+++ dist/umanit-view.js
@@ -10,35 +10,35 @@ app.directive('umanitView', function() {
     restrict: 'A',
     link: function(scope, element, attrs) {
       if (attrs.onLoaded) {
-        scope.$parent.$on('$ionicView.loaded', function() { scope.$eval(attrs.onLoaded); });
+        scope.$on('$ionicView.loaded', function() { scope.$eval(attrs.onLoaded); });
       }
 
       if (attrs.onEnter) {
-        scope.$parent.$on('$ionicView.enter', function() { scope.$eval(attrs.onEnter); });
+        scope.$on('$ionicView.enter', function() { scope.$eval(attrs.onEnter); });
       }
 
       if (attrs.onLeave) {
-        scope.$parent.$on('$ionicView.leave', function() { scope.$eval(attrs.onLeave); });
+        scope.$on('$ionicView.leave', function() { scope.$eval(attrs.onLeave); });
       }
 
       if (attrs.onBeforeEnter) {
-        scope.$parent.$on('$ionicView.beforeEnter', function() { scope.$eval(attrs.onBeforeEnter); });
+        scope.$on('$ionicView.beforeEnter', function() { scope.$eval(attrs.onBeforeEnter); });
       }
 
       if (attrs.onBeforeLeave) {
-        scope.$parent.$on('$ionicView.beforeLeave', function() { scope.$eval(attrs.onBeforeLeave); });
+        scope.$on('$ionicView.beforeLeave', function() { scope.$eval(attrs.onBeforeLeave); });
       }
 
       if (attrs.onAfterEnter) {
-        scope.$parent.$on('$ionicView.afterEnter', function() { scope.$eval(attrs.onAfterEnter); });
+        scope.$on('$ionicView.afterEnter', function() { scope.$eval(attrs.onAfterEnter); });
       }
 
       if (attrs.onAfterLeave) {
-        scope.$parent.$on('$ionicView.afterLeave', function() { scope.$eval(attrs.onAfterLeave); });
+        scope.$on('$ionicView.afterLeave', function() { scope.$eval(attrs.onAfterLeave); });
       }
 
       if (attrs.onUnloaded) {
-        scope.$parent.$on('$ionicView.unloaded', function() { scope.$eval(attrs.onUnloaded); });
+        scope.$on('$ionicView.unloaded', function() { scope.$eval(attrs.onUnloaded); });
       }
     }
   }
